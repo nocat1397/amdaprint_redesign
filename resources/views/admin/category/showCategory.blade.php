09855e4779927e88bb0ea3618b696783dda47421
nocat1397 <nocat_desk@nocat.tech>
2023-05-12 17:26:33 +0530

    category update done

--- resources/views/admin/category/showCategory.blade.php
+++ resources/views/admin/category/showCategory.blade.php
@@ -112,6 +112,7 @@
                         <td>{{str_replace('-',' ',$data->name)}}</td>
                         <td>
                           <div class="btn-group">
+                            <button class="btn btn-success btn-sm mr-2" data-toggle="modal" data-target="#modal-{{$data->id}}"><i class="fas fa-edit"></i></button>
                             <a href="{{url('sub-categories/'.$data->id)}}" class="btn btn-light bg-primary text-light shadow rounded-pill btn-sm">Sub Categories</a>
                           </div>
                         </td>
@@ -173,6 +174,8 @@
 <!-- REQUIRED SCRIPTS -->
 {{-- @include('admin.script') --}}
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 <script>
   $(document).ready(function(){
